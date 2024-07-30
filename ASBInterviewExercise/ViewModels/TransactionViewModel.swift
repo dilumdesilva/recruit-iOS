e2e07f8480c24dccf571fd4f3647f38582534e99
Dilum De Silva <dilumdesilva.info@gmail.com>
2024-07-30 12:37:25 +1200

    feat: vm to fetch transactions

--- ASBInterviewExercise/ViewModels/TransactionViewModel.swift
+++ ASBInterviewExercise/ViewModels/TransactionViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+@MainActor
+final class TransactionsViewModel: ObservableObject {
+    let store: NetworkingStore
+    
+    @Published var transactions: [TransactionModel]?
+    @Published var error: NetworkError?
+    @Published var isLoading = false
+    
+    init(store: NetworkingStore) {
+        self.store = store
+    }
+    
+    func getTransactions() async {
+        isLoading = true
+        let result = await store.getTransactions()
+        switch result {
+        case .success(let transactions):
+            self.transactions = transactions
+            isLoading = false
+        case .failure(let error):
+            self.error = error
+            isLoading = false
+        }
+    }
+}
